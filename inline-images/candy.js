69dfc78c1ec8233af567ccf39627e4e42dc420d8
Manuel Alabor <msites@msites.net>
2011-10-03 14:53:56 +0200

    added initial files for inline-images

---
+++ inline-images/candy.js
@@ -0,0 +1,18 @@
+/*
+ * inline-images
+ * @version 1.0
+ * @author Manuel Alabor (manuel@alabor.me)
+ *
+ * Display an image itself instead of its URL.
+ */
+
+var CandyShop = (function(self) { return self; }(CandyShop || {}));
+
+CandyShop.InlineImages = (function(self, Candy, $) {
+
+	self.init = function() {
+		
+	};
+
+	return self;
+}(CandyShop.InlineImages || {}, Candy, jQuery));
