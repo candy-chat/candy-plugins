bca0154d449a79600bfd0736543939543175137b
Manuel Alabor <msites@msites.net>
2011-10-03 15:06:57 +0200

    comments and some basic structure

--- inline-images/candy.js
+++ inline-images/candy.js
@@ -9,10 +9,29 @@
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.InlineImages = (function(self, Candy, $) {
-
+	
+	var _fileExtensions = ['png','jpg','gif'];
+	
+	/** Function: init
+	 * Initializes the inline-images plugin.
+	 */
 	self.init = function() {
-		
+		Candy.View.Event.Message.beforeShow = handleBeforeShow;
 	};
+	
+	/** Function: handleBeforeShow
+	 * Handles the beforeShow event of a message.
+	 *
+	 * Paramteres:
+	 *   (String) message - the message to process
+	 *
+	 * Returns:
+	 *   (String)
+	 */
+	var handleBeforeShow = function(message) {
+		return message;
+	};
+	
 
 	return self;
 }(CandyShop.InlineImages || {}, Candy, jQuery));
