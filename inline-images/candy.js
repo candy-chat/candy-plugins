c8d1d95fb212d5b4881013adee0e1b058623e923
Manuel Alabor <msites@msites.net>
2011-10-06 15:47:12 +0200

    added demo image for offline testing

--- inline-images/candy.js
+++ inline-images/candy.js
@@ -77,6 +77,7 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	var handleBeforeShow = function(message) {
 		// http://farm6.static.flickr.com/5202/5355145448_93f2233180_t.jpg
 		var processed = message.replace(_imageRegex,'<img src="$1" />');
+		//processed = processed.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|]?(\.jpg))/ig, '<a href="$1" target="_blank">$1</a>');
 		
 		return processed;
 	};
