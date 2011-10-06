ff144c4e2dd60bceff599aefc0b17e43c8e47114
Manuel Alabor <msites@msites.net>
2011-10-06 09:37:58 +0200

    broke down the creation of the image-matching-regex

--- inline-images/candy.js
+++ inline-images/candy.js
@@ -10,8 +10,9 @@ var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.InlineImages = (function(self, Candy, $) {
 	
-	var _fileExtensions = ['png','jpg','gif']
-		,_originalLinkify = Candy.Util.Parser.linkify;
+	var _fileExtensions = ['png','jpg','jpeg','gif']
+		,_originalLinkify = Candy.Util.Parser.linkify
+		,_imageRegex;
 	
 	/** Function: init
 	 * Initializes the inline-images plugin.
@@ -19,7 +20,47 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	self.init = function() {
 		Candy.View.Event.Message.beforeShow = handleBeforeShow;
 		Candy.Util.Parser.linkify = linkify;
+		_imageRegex =  buildImageRegex(_fileExtensions);
 	};
+	
+	/** Function: buildImageRegex
+	 * Creates a RegExp to match URL's to an image inside a string.
+	 * Pass an array with file extensions you want to identify as image.
+	 *
+	 * Paramters:
+	 *   (String Array) forFileExtensions - image file extensions (png, jpg, ...)
+	 *
+	 * Returns:
+	 *   (RegExp)
+	 */
+	var buildImageRegex = function(forFileExtensions) {
+		var pattern = '\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|].';
+		pattern += buildExtensionPattern(forFileExtensions);
+		console.info(pattern);
+		return new RegExp(pattern,'ig');
+	}
+	
+	/** Function: buildExtensionPattern
+	 * Takes an array with file extensions and creates an alternative regex
+	 * pattern out of it.
+	 * Example: ['jpg','png','gif'] => '(jpg|png|gif)'
+	 *
+	 * Paramters:
+	 *   (String array) extensions - File extensions
+	 * 
+	 * Returns:
+	 *  (String)
+	 */
+	var buildExtensionPattern = function(extensions) {
+		var extensions = '(';
+		for(var i = 0, l = _fileExtensions.length; i < l; i++) {
+			extensions += _fileExtensions[i];
+			if(i < l-1) extensions += '|';
+		}
+		extensions += ')';
+		
+		return extensions;
+	}
 	
 	/** Function: handleBeforeShow
 	 * Handles the beforeShow event of a message.
@@ -31,14 +72,15 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	 *   (String)
 	 */
 	var handleBeforeShow = function(message) {
-		var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 		// http://farm6.static.flickr.com/5202/5355145448_93f2233180_t.jpg
 		
-		return message.replace(exp,'<img src="$1" />');
+		return message.replace(_imageRegex,'<img src="$1" />');
 	};
 	
 	/** Function: linkify
 	 * Is used to overwrite the original Candy.Util.Parser.linkify.
+	 * This implementation prevents the parsing of URL's by the Candy core.
+	 * inline-images handles this on itself by handleBeforeShow.
 	 *
 	 * Parameters:
 	 *   (String) text - text to process
@@ -47,7 +89,7 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	 *   (String)
 	 */
 	var linkify = function(text) {
-		return _originalLinkify(text);
+		return text;
 	}
 	
 
