1c6953f5f23500e066e6ce0bb6314fa822c12a9f
Manuel Alabor <msites@msites.net>
2011-10-06 09:04:56 +0200

    added overwrite for Candy.Util.Parser.linkify

--- inline-images/candy.js
+++ inline-images/candy.js
@@ -10,13 +10,15 @@ var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.InlineImages = (function(self, Candy, $) {
 	
-	var _fileExtensions = ['png','jpg','gif'];
+	var _fileExtensions = ['png','jpg','gif']
+		,_originalLinkify = Candy.Util.Parser.linkify;
 	
 	/** Function: init
 	 * Initializes the inline-images plugin.
 	 */
 	self.init = function() {
 		Candy.View.Event.Message.beforeShow = handleBeforeShow;
+		Candy.Util.Parser.linkify = linkify;
 	};
 	
 	/** Function: handleBeforeShow
@@ -29,8 +31,24 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	 *   (String)
 	 */
 	var handleBeforeShow = function(message) {
-		return message;
+		var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+		// http://farm6.static.flickr.com/5202/5355145448_93f2233180_t.jpg
+		
+		return message.replace(exp,'<img src="$1" />');
 	};
+	
+	/** Function: linkify
+	 * Is used to overwrite the original Candy.Util.Parser.linkify.
+	 *
+	 * Parameters:
+	 *   (String) text - text to process
+	 *
+	 * Returns:
+	 *   (String)
+	 */
+	var linkify = function(text) {
+		return _originalLinkify(text);
+	}
 	
 
 	return self;
