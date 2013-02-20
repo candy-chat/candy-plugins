3cf18fe699d0942deff3c9456d79e18102a780fb
Drew Harry <drew.harry@gmail.com>
2013-02-20 12:31:32 -0500

    Added a barebones candy.js file.

---
+++ replies/candy.js
@@ -0,0 +1,17 @@
+/*
+ * candy-replies-plugin
+ * @version 0.1 (2013-2-20)
+ * @author Drew Harry (drew.harry@gmail.com)
+ *
+ * Adds @reply highlighting to chat messages to help with high velocity 
+ * conversations.
+ */
+
+var CandyShop = (function(self) { return self; }(CandyShop || {}));
+
+CandyShop.Replies = (function(self, Candy, $) {
+
+	self.init = function() {
+    
+	return self;
+}(CandyShop.Replies || {}, Candy, jQuery));
