5157452d5b97333cdfd80db5bfaee6091460c4e7
Michael Weibel <michael.weibel@gmail.com>
2013-05-07 21:05:41 +0200

    Fix #43 - many thanks @warcode

--- inline-images/candy.js
+++ inline-images/candy.js
@@ -65,21 +65,22 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 
 
 	/** Function: handleBeforeShow
-         * Handles the beforeShow event of a message.
-         *
-         * Paramteres:
-         *   (Object) args - {roomJid, element, nick, message}
-         *
-         * Returns:
-         *   (String)
-         */
-        var handleBeforeShow = function(args) {
-                var message = args.message;
-                var processed = message.replace(/\|[^\|]+\|/, "");
-                processed = processed.replace(/(^|[^\/])(www\.[^\.]+\.[\S]+(\b|$))/gi, '$1http://$2');
-                processed = processed.replace(/\b(https?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, replaceCallback);
-                return processed;
-        };
+	 * Handles the beforeShow event of a message.
+	 *
+	 * Paramteres:
+	 *   (Object) args - {roomJid, element, nick, message}
+	 *
+	 * Returns:
+	 *   (String)
+	 */
+	var handleBeforeShow = function(e, args) {
+		var message = args.message;
+		var processed = message.replace(/\|[^\|]+\|/, "");
+		processed = processed.replace(/(^|[^\/])(www|i\.[^\.]+\.[\S]+(\b|$))/gi, '$1http://$2');
+		processed = processed.replace(/\b(https?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, replaceCallback);
+		args.message = processed;
+		return processed;
+	};
 
 
 	/** Function: handleOnShow
@@ -92,7 +93,7 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 	 * Parameters:
 	 *   (Array) args
 	 */
-	var handleOnShow = function(args) {
+	var handleOnShow = function(e, args) {
 		$('.inlineimages-loader').each(function(index, element) {
 			$(element).removeClass('inlineimages-loader');
 			var url = $(element).attr('longdesc');
@@ -101,9 +102,11 @@ CandyShop.InlineImages = (function(self, Candy, $) {
 			$(imageLoader).load(function() {
 				var origWidth = this.width;
 				var origHeight = this.height;
-				var ratio = Math.min(_maxImageSize / origWidth, _maxImageSize / origHeight);
-				var width = Math.round(ratio * origWidth);
-				var height = Math.round(ratio * origHeight);
+				if(origWidth > _maxImageSize || origHeight > _maxImageSize) {
+					var ratio = Math.min(_maxImageSize / origWidth, _maxImageSize / origHeight);
+					var width = Math.round(ratio * origWidth);
+					var height = Math.round(ratio * origHeight);
+				}
 
 				$(element).replaceWith(buildImageSource(url, width, height))
 			});
