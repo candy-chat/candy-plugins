79c62937e4d97f2cbab5d406c9b317a5e1d7a2aa
Patrick Stadler <patrick.stadler@gmail.com>
2014-01-05 11:29:49 +0100

    fix "replies" plugin to use new event system

--- replies/candy.js
+++ replies/candy.js
@@ -1,9 +1,9 @@
 /*
  * candy-replies-plugin
- * @version 0.1 (2013-2-20)
+ * @version 0.2 (2014-01-05)
  * @author Drew Harry (drew.harry@gmail.com)
  *
- * Adds @reply highlighting to chat messages to help with high velocity 
+ * Adds @reply highlighting to chat messages to help with high velocity
  * conversations.
  */
 
@@ -12,24 +12,23 @@ var CandyShop = (function(self) { return self; }(CandyShop || {}));
 CandyShop.Replies = (function(self, Candy, $) {
 
 	self.init = function() {
-	  
-    Candy.View.Event.Message.onShow = handleOnShow;
-    
+
+    $(Candy).on('candy:view.message.after-show', handleOnShow);
+
 	  return self;
   };
-  
-  var handleOnShow = function(args) {
+
+  var handleOnShow = function(e, args) {
     var localNick = Candy.Core.getUser().getNick();
-    
+
     var re = new RegExp("@" + localNick + "([ .!><\":\/@-]|$)", 'im');
-    
+
     if(re.test(args.message)) {
       var el = args.element;
 
       el.addClass("mention");
-      el.prev().addClass("mention");
     }
   }
-  
+
   return self;
 }(CandyShop.Replies || {}, Candy, jQuery));
