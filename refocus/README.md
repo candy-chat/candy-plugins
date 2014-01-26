e0fb9a91009e552ce66d7abe6c52c4557da6dbf5
warcode <warcode@deny.io>
2014-01-26 17:19:08 +0100

    Renamed to Refocus
    Removed css file.
    Updated README.

--- refocus/README.md
+++ refocus/README.md
@@ -1,12 +1,11 @@
 # Automatic entry box focus
 
-This plugin puts the focus on the entry box if the user clicks in the message window.
+This plugin puts the focus on the entry box if the user clicks somewhere in the message list.
 
 ## Usage
 
 ```HTML
 <script type="text/javascript" src="candyshop/autofocus/candy.js"></script>
-<link rel="stylesheet" type="text/css" href="candyshop/autofocus/candy.css" />
 ```
 
 ```JavaScript
--- refocus/candy.js
+++ refocus/candy.js
@@ -1,32 +1,32 @@
 /*
-* candy-autofocus-plugin
-* @version 0.1 (2013-5-8)
+* candy-refocus-plugin
+* @version 1.0 (2014-01-26)
 * @author warcode (github.com/warcode)
 *
-* This plugin puts the focus on the entry box if the user clicks in the message window.
+* This plugin puts the focus on the entry box if the user clicks in the message window/list.
 */
 
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
-CandyShop.Autofocus = (function(self, Candy, $) {
+CandyShop.Refocus = (function(self, Candy, $) {
     
     self.init = function(options) {
-        Candy.Core.log('[Autofocus] init');
+        Candy.Core.log('[Refocus] init');
         $(Candy.View.Pane).on('candy:view.room.after-show', roomAfterShow);
     };
 
     function roomAfterShow(e, args) {
-        Candy.Core.log('[Autofocus] roomAfterShow');
+        Candy.Core.log('[Refocus] roomAfterShow');
 	try {
             $('.message-pane-wrapper').mousedown(function() {
                 $('.message-form').children(".field")[0].focus();
                 return false;
             });
         } catch (e) {
-            Candy.Core.log('[Autofocus] jQuery exception:');
+            Candy.Core.log('[Refocus] jQuery exception:');
             Candy.Core.log(e);
         }
     }
 
   return self;
-}(CandyShop.Autofocus || {}, Candy, jQuery));
+}(CandyShop.Refocus || {}, Candy, jQuery));
