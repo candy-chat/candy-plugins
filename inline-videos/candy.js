31b268b48fd48360f506b2c5fe1f9c51cdbef8d6
Leho Kraav <leho@kraav.com>
2014-10-07 11:07:32 +0300

    inline-videos: switch to protocol-relative iframe URI to avoid mixed content issues

--- inline-videos/candy.js
+++ inline-videos/candy.js
@@ -32,7 +32,7 @@ CandyShop.InlineVideos = (function(self, Candy, $) {
 	 *   (String)
 	 */
 	self.handleBeforeShow = function(e, args) {
-		args.message = args.message.replace(/\>(https?:\/\/w{0,3}\.?youtube.com\/watch\?v=([^\s^&]*)([^\s]*))\<\/a\>/i, '>$1<br /><iframe width="300" height="200" src="http://www.youtube.com/embed/$2" frameborder="0" allowfullscreen></iframe></a><br />');
+		args.message = args.message.replace(/\>(https?:\/\/w{0,3}\.?youtube.com\/watch\?v=([^\s^&]*)([^\s]*))\<\/a\>/i, '>$1<br /><iframe width="300" height="200" src="//www.youtube.com/embed/$2" frameborder="0" allowfullscreen></iframe></a><br />');
 	};
 
 	return self;
