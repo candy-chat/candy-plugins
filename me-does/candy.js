a368b1ea86c7f6d24af5b55380842b60857bc6f2
Michael Weibel <michael.weibel@gmail.com>
2015-07-13 08:00:42 +0200

    Fix me does
    Fixes #139

--- me-does/candy.js
+++ me-does/candy.js
@@ -6,7 +6,7 @@ CandyShop.MeDoes = (function(self, Candy, $) {
 		$(Candy).on("candy:view.message.before-show", function(e, args) {
 			if (args && args.message && args.message.match(/^\/me /i)) {
 				var message = args.message.match(/^\/([^\s]+)(?:\s+(.*))?$/m)[2];
-				Candy.View.Pane.Chat.infoMessage(args.roomJid, '<span><strong>' + args.name + '</strong> ' + message + '</span>');
+				Candy.View.Pane.Chat.infoMessage(args.roomJid, null, '<span><strong>' + args.name + '</strong> ' + message + '</span>');
 				return false;
 			}
 		});
