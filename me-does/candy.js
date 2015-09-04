c2ddf64411f4c33b49df24de25660277237a8900
Sudrien <_+github@sudrien.net>
2015-09-04 00:30:01 -0400

    The rest of #144 - unique function naming, Timeago support

--- me-does/candy.js
+++ me-does/candy.js
@@ -1,25 +1,34 @@
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.MeDoes = (function(self, Candy, $) {
+// CandyShop.Timeago
+
 
 	self.init = function() {
 		$(Candy).on("candy:view.message.before-show", function(e, args) {
 			if (args && args.message && args.message.match(/^\/me /i)) {
 				var message = args.message.match(/^\/([^\s]+)(?:\s+(.*))?$/m)[2];
-				self.infoMessage(args.roomJid, args.name, message);
+				self.userInfoMessage(args.roomJid, args.name, message);
 				return false;
 			}
 		});
 
 	};
 	
-	self.meDoesTemplate = '<li><small>{{time}}</small><div class="infomessage">' +
-	'<span class="spacer">•</span>&nbsp;<span><strong>{{name}}</strong>&nbsp;{{message}}</span></div></li>';
+        if(CandyShop.Timeago === undefined) {
+		Candy.View.Template.Chat.userInfoMessage = '<li><small>{{time}}</small><div class="infomessage">' +
+		'<span class="spacer">•</span>&nbsp;<span><strong>{{name}}</strong>&nbsp;{{message}}</span></div></li>';
+	}
+	else {
+		Candy.View.Template.Chat.userInfoMessage = '<li><small><abbr title="{{time}}">{{time}}</abbr></small><div class="infomessage">' +
+		'<span class="spacer">•</span>&nbsp;<span><strong>{{name}}</strong>&nbsp;{{message}}</span></div></li>';
+	}
 	
 	//Using logic from real infoMessage function and inserting custom template
-	self.infoMessage = function (roomJid, name, message){
+	self.userInfoMessage = function (roomJid, name, message){
+
 		if(Candy.View.getCurrent().roomJid) {
-			var html = Mustache.to_html(self.meDoesTemplate, {
+			var html = Mustache.to_html(Candy.View.Template.Chat.userInfoMessage, {
 				name: name,
 				message: message,
 				time: Candy.Util.localizedTime(new Date().toGMTString())
@@ -28,6 +37,9 @@ CandyShop.MeDoes = (function(self, Candy, $) {
 			if (Candy.View.getCurrent().roomJid === roomJid) {
 				Candy.View.Pane.Room.scrollToBottom(Candy.View.getCurrent().roomJid);
 			}
+			$(Candy).triggerHandler('candy:view.message.after-show', {
+				'message' : message
+			});
 		}
 	};
 	
