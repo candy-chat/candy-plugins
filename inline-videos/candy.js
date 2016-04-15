5fca467528bba2e261425cdcc4a2868a0221d3f9
Sudrien <_+github@sudrien.net>
2016-04-15 00:27:21 -0400

    Fixes #156, handles xhtmlMessage, better support for multiple videos

--- inline-videos/candy.js
+++ inline-videos/candy.js
@@ -24,10 +24,9 @@ CandyShop.InlineVideos = (function(self, Candy, $) {
 	self.init = function() {
 		// add a listener to these events
 		$(Candy).on('candy:view.message.before-show', self.handleBeforeShow);
+		self.appender = "";
 	};
 
-	
-	
 	/** Function: timeToSeconds
 	 * converts youtube's t= parameter value to seconds
 	 *
@@ -52,9 +51,97 @@ CandyShop.InlineVideos = (function(self, Candy, $) {
 			ret += times[6] ? parseInt(times[6]) : 0; //seconds
 		}
 		return ret;
-	};
+	};	
 
-	
+	/** Function: linkReplace
+	 * The logic to our link replacement, which uses browser link parsing
+	 *
+	 * Parameters:
+	 *   (String) match - full match, requirement for call & return value in case of failure
+	 *   (String) href - url to parse
+	 *
+	 * Returns:
+	 *   (String) - The original link, or possibly a cleaned up link
+	 *
+	 * Side Effect:
+	 *   (String) - self.appender - Can't return the embeded movie by normal means, because
+	 *              order gets messed up. So put it elswhere so we can append later.
+	 */
+	var linkReplace = function(match, href) {
+		var a = document.createElement('a');
+		a.href = href;
+
+		switch(a.hostname) { // a link with an href
+		case "youtube.com":
+		case "www.youtube.com":
+		case "youtu.be":
+			if(a.pathname == "/watch") {
+				var v = a.search.match(/v=([a-zA-Z0-9_-]{11})/);
+			}
+			else {
+				var v = a.pathname.match(/^\/([a-zA-Z0-9_-]{11})/);
+			}
+			var start = timeToSeconds(a.search.match(/t=([hms0-9]+)/));
+			var list = a.search.match(/list=([^&]+)/);
+			var main_url = "https://youtu.be/" + (v ? v[1] : "");
+			var embed_url = "https://www.youtube.com/embed/" + (v ? v[1] : "");
+
+			if(start || list) {
+				main_url += "?";
+				embed_url += "?";
+			}
+			if(start > 0) {
+				main_url += "t=" + start;
+				embed_url += "start=" + start;
+			}
+			if(start && list) {
+				main_url += "&";
+				embed_url += "&";
+			}
+			if(list) {
+				main_url += "list=" + list[1];
+				embed_url += "list=" + list[1];
+			}
+			var aspect = "16by9";
+			break;
+		case "vimeo.com":
+		case "player.vimeo.com":
+			if(a.pathname.match(/video/)) {
+				var v = a.pathname.match(/\/video\/(\d+)/);
+			}
+			else {
+				var v = a.pathname.match(/^\/(\d+)/);
+			}
+			var main_url = "https://vimeo.com/" + (v ? v[1] : "");
+			var embed_url = "https://player.vimeo.com/video/" + (v ? v[1] : "");
+			var aspect = "16by9";
+			break;
+		case "dailymotion.com":
+		case "www.dailymotion.com":
+			if(a.pathname.match(/video\/x/)) {
+				var v = a.pathname.match(/\/video\/(x[\da-z]+)(_|$)/);
+			}
+			var main_url = "https://www.dailymotion.com/video/" + (v ? v[1] : "");
+			var embed_url = "https://www.dailymotion.com/embed/video/" + (v ? v[1] : "");
+			var aspect = "16by9";
+			break;
+		case "vine.co":
+		case "www.vine.co":
+			if(a.pathname.match(/v\//)) {
+				var v = a.pathname.match(/v\/([\da-z]+)(\/|$)/i);
+			}
+			var main_url = "https://vine.co/v/" + (v ? v[1] : "");
+			var embed_url = "https://vine.co/v/" + (v ? v[1] : "") + "/embed/simple";
+			var aspect = "1by1";
+			break;
+		}
+		
+		if (v) {
+			self.appender += "<div class='inline-video inline-video-" + aspect + "'><iframe src='" + embed_url + "' allowfullscreen='true'></iframe></div> ";
+			return "<a href='" + main_url + "'>" + main_url + "</a>";
+			}
+		return match;
+		}
 	
 	/** Function: handleBeforeShow
 	 * Handles the beforeShow event of a message.
@@ -66,93 +153,26 @@ CandyShop.InlineVideos = (function(self, Candy, $) {
 	 *   (String)
 	 */
 	self.handleBeforeShow = function(e, args) {
-		args.message = jQuery.parseHTML(args.message).map(function(a) {
-			if(a.outerHTML){ //dom element of any sort
-				switch(a.hostname) { // a link with an href
-				case "youtube.com":
-				case "www.youtube.com":
-				case "youtu.be":
-					if(a.pathname == "/watch") {
-						var v = a.search.match(/v=([a-zA-Z0-9_-]{11})/);
-					}
-					else {
-						var v = a.pathname.match(/^\/([a-zA-Z0-9_-]{11})/);
-					}
-					var start = timeToSeconds(a.search.match(/t=([hms0-9]+)/));
-					var list = a.search.match(/list=([^&]+)/);
-					var main_url = "https://youtu.be/" + (v ? v[1] : "");
-					var embed_url = "https://www.youtube.com/embed/" + (v ? v[1] : "");
+		self.appender = "";
 
-					if(start || list) {
-						main_url += "?";
-						embed_url += "?";
-					}
-					if(start > 0) {
-						main_url += "t=" + start;
-						embed_url += "start=" + start;
-					}
-					if(start && list) {
-						main_url += "&";
-						embed_url += "&";
-					}
-					if(list) {
-						main_url += "list=" + list[1];
-						embed_url += "list=" + list[1];
-					}
-					var aspect = "16by9";
-					break;
-				case "vimeo.com":
-				case "player.vimeo.com":
-					if(a.pathname.match(/\/video\//)) {
-						var v = a.pathname.match(/\/video\/(\d+)/);
-					}
-					else {
-						var v = a.pathname.match(/^\/(\d+)/);
-					}
-					var main_url = "https://vimeo.com/" + (v ? v[1] : "");
-					var embed_url = "https://player.vimeo.com/video/" + (v ? v[1] : "");
-					var aspect = "16by9";
-					break;
-				case "dailymotion.com":
-				case "www.dailymotion.com":
-					if(a.pathname.match(/\/video\/x/)) {
-						var v = a.pathname.match(/\/video\/(x[\da-z]+)(_|$)/);
-					}
-					var main_url = "https://www.dailymotion.com/video/" + (v ? v[1] : "");
-					var embed_url = "https://www.dailymotion.com/embed/video/" + (v ? v[1] : "");
-					var aspect = "16by9";
-					break;
-				case "vine.co":
-				case "www.vine.co":
-					if(a.pathname.match(/^\/v\//)) {
-						var v = a.pathname.match(/^\/v\/([\da-z]+)(\/|$)/i);
-					}
-					var main_url = "https://vine.co/v/" + (v ? v[1] : "");
-					var embed_url = "https://vine.co/v/" + (v ? v[1] : "") + "/embed/simple";
-					var aspect = "1by1";
-					break;
-				}
-				
-				if (v) {
-					return "<a href='" + main_url + "'>" + main_url + "<br />\
-					<div class='inline-video inline-video-" + aspect + "'>\
-					<iframe src='" + embed_url + "' allowfullscreen='true'>\
-					</iframe>\
-					</div>\
-					</a>";
-				}
-				else {
-					return a.outerHTML;
-				}
-				
-				
+		if(args.xhtmlMessage) {
+			args.xhtmlMessage = args.xhtmlMessage.replace(/\<a href="(.+?)".+?\<\/a\>/g, linkReplace);
+			if(self.appender != "") {
+				args.xhtmlMessage += "<br>";
+				args.xhtmlMessage += self.appender;
 			}
-			else { //this is a string that has to be wrapped in somthing
-				return a.textContent;
-			}
-		}).join("");
+		}
+
+		self.appender = "";
+
+		args.message = args.message.replace(/\<a href="(.+?)".+?\<\/a\>/g, linkReplace);
+		if(self.appender != "") {
+			args.message += "<br>";
+			args.message += self.appender;
+		}
 	};
 
 	return self;
 }(CandyShop.InlineVideos || {}, Candy, jQuery));
+
 
